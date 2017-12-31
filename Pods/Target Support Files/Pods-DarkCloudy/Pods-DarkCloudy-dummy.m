d12a4d5e07b3a89aed9e67d855c8601f0b270f64
Scott Moon <jiwonsis@naver.com>
2017-12-31 20:13:51 +0900

    setting Carthage

--- Pods/Target Support Files/Pods-DarkCloudy/Pods-DarkCloudy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DarkCloudy : NSObject
-@end
-@implementation PodsDummy_Pods_DarkCloudy
-@end
--- Pods/Target Support Files/Pods-DarkCloudyTests/Pods-DarkCloudyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DarkCloudyTests : NSObject
-@end
-@implementation PodsDummy_Pods_DarkCloudyTests
-@end
